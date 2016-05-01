3ca3b54746a7acb947ae12a19299d80efea89e7d
Mina Lee <minalee@cs.utexas.edu>
2016-05-01 15:19:06 -0500

    yay

--- cstuff.c
+++ cstuff.c
@@ -29,33 +29,6 @@ int* multiply(){
     return *c;
 }
 
-int* multiply2() {
-    static int c[N][N];
-    register int blockSize = 8;
-    int sum = 0 ;
-
-    for(int x = 0; x < N; x+=blockSize){
-        for(int y = 0; y < N; y++){
-            for(int z = x; z < x+blockSize && z < N; z++){
-                c[y][z] = 0;
-            }
-        }
-        for(int h = 0; h < N; h += blockSize){
-            for(int i=0; i < N; i++ ){
-                for(int j = x; j < x + blockSize && j < N; j++){
-	            sum = 0;	
-                    for(int k = h; k < h + blockSize && k < N; k++){
-                        sum += ma[i][k] * mb[k][j];
-                    }
-                    c[i][j] += sum;
-                }
-            }
-        }
-    }
-
-    return *c;
-}
-
 int* naive(void){
     static int c[N][N];
 
